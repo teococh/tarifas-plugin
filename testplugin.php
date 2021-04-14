395544251dcf02510c5e9a249d3b9fc1cc9663c3
Teo <55553715+teococh@users.noreply.github.com>
2021-04-14 11:15:58 +0200

    submit

--- testplugin.php
+++ testplugin.php
@@ -8,14 +8,30 @@ Version: 0.0.1
 */
 
 function Activar() {
-    echo "<h1>ASDASDASDASDASDASDAS</h1>";
+   
 }
 
 function Desactivar() {
 
 }
 
+add_action('admin_menu','CrearMenu');
 
+function CrearMenu(){
+    add_menu_page(
+        'tarifas', // Nombre de la pagina
+        'sferaTarifas', // Nombre del menu
+        'manage_options', // CApability
+        'sp_menu', // Slug
+        'MostrarContenido', // Funcion del contenido
+            plugin_dir_url(__FILE__).'admin/img/icono.png',
+            '1'
+    );
+}
+
+function MostrarContenido() {
+    echo "<h1>Contenido de la pagina</h1>";
+}
 
 register_activation_hook(__FILE__, 'Activar');
 register_deactivation_hook(__FILE__, 'Desactivar');
