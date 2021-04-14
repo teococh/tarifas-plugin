d2b3e12a5a8d4b1672db187df0627b5421db29f3
Teo <55553715+teococh@users.noreply.github.com>
2021-04-14 18:01:50 +0200

    Formulario principal
    Empezando a hacer el formulario para generar nuevas tarifas

---
+++ admin/principal.php
@@ -0,0 +1,20 @@
+<h1>ASDASDASDASDASDASDASD</h1>
+
+    <form action="" method="post">
+        <fieldset>
+            <legend>Tarifas</legend>
+            <label for="">Tipo de tarifa</label>
+            <select name="tipo" id="tipo">
+                <option value="📱">Movil</option>
+                <option value="📶">Fibra + fijo</option>
+                <option value="3">Movil compartido</option>
+            </select>
+            <div id="content"> 
+            </div>
+        </fieldset>
+    </form>
+    <button id="btn-añadir">Añadir</button>
+
+    <!-- js personalizado se encuentra en: htdocs\wp-admin\js\script-sfera-plugin.js -->
+
+<script src="js\script-sfera-plugin.js"></script>
--- testplugin.php
+++ testplugin.php
@@ -9,6 +9,12 @@ Version: 0.0.1
 
 function Activar() {
    
+
+    // TODO Conexion con la base de datos para crear las tablas necesarias 
+    global $wpdb;
+
+    
+
 }
 
 function Desactivar() {
@@ -22,16 +28,30 @@ function CrearMenu(){
         'tarifas', // Nombre de la pagina
         'sferaTarifas', // Nombre del menu
         'manage_options', // CApability
-        'sp_menu', // Slug
-        'MostrarContenido', // Funcion del contenido
-            plugin_dir_url(__FILE__).'admin/img/icono.png',
+        plugin_dir_path(__FILE__).'admin/principal.php', // Slug
+        null, // Funcion del contenido
+            plugin_dir_url(__FILE__).'admin/img/icono.svg',
             '1'
+    );
+    
+    add_submenu_page(
+        'sp_menu', // parent slug
+        'Ajustes', // Titulo
+        'Ajustes', // Titulo menu
+        'manage_options', //
+        'sp_menu_ajustes',
+        'Submenu'
     );
 }
 
 function MostrarContenido() {
     echo "<h1>Contenido de la pagina</h1>";
 }
+
+function Submenu() {
+    echo "pruebas";
+}
+
 
 register_activation_hook(__FILE__, 'Activar');
 register_deactivation_hook(__FILE__, 'Desactivar');
